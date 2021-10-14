21ca3ed9221a5101eec996d42c7a45232ffab219
Wilfredo Murillo <wilmugo@gmail.com>
2021-10-14 18:04:09 -0400

    Agregados los modelos de leaders y promotions a la db y modificadas las rutas de los mismos: Assignment 2

---
+++ models/leaders.js
@@ -0,0 +1,35 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const leadersSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  image: {
+    type: String,
+    required: true,
+  },
+  designation: {
+    type: String,
+    required: true,
+  },
+  abbr: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  featured: {
+    type: Boolean,
+    default: false,
+  },
+});
+
+const Leaders = mongoose.model("Leader", leadersSchema);
+
+module.exports = Leaders;
